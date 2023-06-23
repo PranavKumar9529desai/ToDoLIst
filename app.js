88f1f993dc84f78804f67c821671cecc696cd0e3
PranavKumar9529desai <dpranav7745@gmail.com>
2023-06-23 12:09:23 +0530

    nealy perfected the Todolist deleted all the unnessary complecations

--- app.js
+++ app.js
@@ -10,113 +10,81 @@ app.use(express.static("public"));
 app.use(bodyparser.urlencoded({extended:true}));
 
 
-// section mongoose
+       // section mongoose
 mongoose.connect('mongodb://127.0.0.1:27017/ToDoListDB');
+
        //creating Schema
-const ToDoListSchema = new mongoose.Schema({
-  name : String
-});
-       // crating Model
-const Task = mongoose.model("Task",ToDoListSchema);
-
-// const task1= new Task ({name : "shopping"});
-// const task2 = new Task ({name : "playing"});
-
-Task.insertMany([{name: 'Workout'} , {name : 'Daily journal'} , {name : 'coding'}]);
-// task1.save();
-// task2.save();
-// task.save();
-
-
-
-
-
-// // let workitem=[];
-// var today = new Date(); 
-// var currentday = today.getDay();
-
-// var day = "";
-// // var newitem;
-
-// if (today.getDay()=== 6 || today.getDay() ===0){
-// currentday = "workoff day";
-
-// }
-// else{
-//     currentday= 'go to work now';
-   
-// }
-// var options = {
-//   weekday: 'long',
-//   day: 'numeric', 
-//   month: 'long'
-// }
-
-// var format = today.toLocaleDateString('en-us',options);
-// var items = [];
-
-//   app.get("/", function(req,res){
-//     Task.find({},function(err,foundtasks){
-//       console.log(foundtasks)  });
-    
-//       console.log("server is running perfectly");
-
-//     res.render("list",{listtitle:format,Task:items});
-// });
-
- 
-
-// app.get("/work" , function(req,res){
-// res.render("list",{listtitle:"work list" , items: workitem})  
-// })
-// app.post("/work" ,function(req,res){
-//   console.log(req.body);
-
-//   workitem.push(items);
-//   res.redirect("/work");
-  
-// });
-
-
-
-
-
-// app.listen(3000,function(){
-//     console.log("server is runnig on 3000");
-// });
-
-//code from comments
 const itemsSchema = new mongoose.Schema({
   name: String
 });
-const workItemsSchema = new mongoose.Schema({
-  name: String
-});
 
-
-
+        //crating a Model
 const Item = mongoose.model("Item", itemsSchema);
-const WorkItem = mongoose.model("Work Item", workItemsSchema);
-const item1 = new Item({ name: "Test item1" });
-const item2 = new Item({ name: "Test item2" });
-const item3 = new Item({ name: "Test item3" });
+const item1 = new Item({ name: "Workout" });
+const item2 = new Item({ name: "Meeting" });
+const item3 = new Item({ name: "Journal Writting" });
  
-const workItem1 = new WorkItem({ name: "Work Test item1" });
-const workItem2 = new WorkItem({ name: "Work Test item2" });
-const workItem3 = new WorkItem({ name: "Work Test item3" });
- 
+
+         //default test items
 const defaultItems = [item1, item2, item3];
-const defaultWorkItems = [workItem1, workItem2, workItem3];
+
+
+
+// // creating new list 
+// const ListSchema = new mongoose.Schema({
+//     name : String,
+//     item : [itemsSchema]
+
+// });
+
+
+// //finding the route 
+
+// app.get("/:CustomListName",function(req,res){
+    
+// const CustomListName= req.params.CustomListName;
+// console.log(CustomListName);
+// const List = mongoose.model(CustomListName , ListSchema);
+
+
+
+
+
+// // Checking whether the list already exists or not
+// List.exists({name: CustomListName})
+//   .then(result => {
+//     if (result) {
+//       console.log('already exist');
+//     } else {
+//       // Creating a new list document
+//       const List = mongoose.model("List" , ListSchema);
+
+//       const newList = new List({name: CustomListName} , {item : defaultItems});
+//       // Saving the new list document
+//       newList.save()
+//         .then(() => console.log('new list saved'))
+//         .catch(err => console.error(err));
+//         // res.render("list" , { listTitle : CustomListName , newItems:  defaultItems})
+//         res.redirect("/" + CustomListName);
+//     }
+//   })
+//   .catch(err => console.error(err));
+
+
+
+// });
+
+
 
 //adding new items to list
 
-                    app.post("/" , function(req,res){
-                     const ItemName = req.body.list_item_1 ;
-                        const item = new Item ({name : ItemName});
-                        item.save();  
-                        console.log(ItemName);
-                        res.redirect("/");
-                    });
+app.post("/" , function(req,res){
+const ItemName = req.body.list_item_1 ;
+const item = new Item ({name : ItemName});
+item.save();  
+console.log(ItemName);
+res.redirect("/");
+});
                     
 // deleting the completed tasks
 app.post("/delete",function(req,res){
@@ -134,6 +102,12 @@ Item.deleteMany({ _id: { $in: req.body.checkbox } })
 
   
 });
+   
+
+// getting date and time 
+var today = new Date();
+var options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+var formattedDate = today.toLocaleDateString("en-US", options); //Friday, June 23, 2023
 
 
 app.get("/", async function (req, res) {
@@ -149,48 +123,19 @@ app.get("/", async function (req, res) {
     } catch (err) {
         console.log(err);
     };
-    res.render("list", { listTitle: "Today", newItems: foundItems });
+    res.render("list", { listTitle: formattedDate, newItems: foundItems });
 });
  
-app.get("/work", async function (req, res) {
-    let foundWorkItems = await WorkItem.find({});
- 
-    try {
-        if (foundWorkItems.length === 0) {
-            foundWorkItems = await WorkItem.insertMany(defaultWorkItems)
-        }
- 
-    } catch (err) {
-        console.log(err);
-    };
-    res.render("list", { listTitle: "Work", newItems: foundWorkItems });
- 
-});
 
 
-//posting new_items in database
 
  
-app.post("/", async function (req, res) {
-    try {
-        let item = req.body.newItem;
-        if (req.body.list === "Work") {
-            await WorkItem.create({ name: item });
-            res.redirect("/work");
-        } else {
-            await Item.create({ name: item });
-            res.redirect("/");
-        };
-    } catch (err) {
-        console.log(err);
-    }
+
  
-});
- 
-app.get("/about", function (req, res) {
-    res.render("about");
-})
+
  
 app.listen(4000, function () {
     console.log("The app is running on the port 4000");
+    
 });
+
